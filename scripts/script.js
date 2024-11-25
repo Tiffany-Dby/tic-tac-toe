d4bd61e5549063d34c7c62c21631cbda5ec5477e
Mehmet Minnet <mehmet.minnet@supdevinci-edu.fr>
2024-11-25 11:44:35 +0100

    add : game logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const squares = document.querySelectorAll("square");
-    const player = 1;
+    const squares = document.querySelectorAll(".square");
+    const resetBtn = document.querySelector(".reset");
+
+    let player = 1;
     function crossthrough(event, square){
+        //if(!!square.textContent.length) return;
+        if(square.textContent.length > 0) return; //if not empty return
+
         if (player % 2 === 0){
             square.textContent = "x";
         }else{
@@ -10,8 +15,22 @@ document.addEventListener("DOMContentLoaded", () => {
         player += 1;
     }
     
+    
+    function reset(event){
+        player = 1;
+        squares.forEach(square => {
+            square.textContent = "";
+        })
+    }
+
     squares.forEach(square  => {
-        square.addEventListener('click', crossthrough);
+        square.addEventListener('click', (e)=>{
+            crossthrough(e,square)
+        });
     });
+
+    resetBtn.addEventListener('click', (e)=>{
+        reset(e)
+    })
 
 });
