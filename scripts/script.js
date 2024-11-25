63bfa223c36a0aca594094665ecbb6a0e3ab946f
Mehmet Minnet <mehmet.minnet@supdevinci-edu.fr>
2024-11-25 13:23:31 +0100

    update winner

--- scripts/script.js
+++ scripts/script.js
@@ -31,5 +31,30 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
+  function winGame() {
+    const winningCombinations = [
+      [0, 1, 2], // Row 1
+      [3, 4, 5], // Row 2
+      [6, 7, 8], // Row 3
+      [0, 3, 6], // Column 1
+      [1, 4, 7], // Column 2
+      [2, 5, 8], // Column 3
+      [0, 4, 8], // Diagonal 1
+      [2, 4, 6], // Diagonal 2
+    ];
+
+    const squaresArray = Array.from(squares);
+
+    // for (const combination of winningCombinations) {
+    //   const [a, b, c] = combination;
+    //   if (
+
+    //   ) {
+    //     alert(`Player ${} wins!`);
+    //     return;
+    //   }
+    // }
+  }
+
   resetBtn.addEventListener("click", reset);
 });
